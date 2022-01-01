7f3431c04d58132597f9349a2d851b95189c7fc7
aliassaad66 <alias_saad66@yahoo.com>
2022-01-02 00:00:59 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let searchBtn = document.querySelector("#search-btn");
+let searchBar = document.querySelector(".search-bar-container");
+
+searchBtn.addEventListener("click", () =>{
+    searchBtn.classList.toggle("fa-times");
+    searchBar.classList.toggle("active");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
