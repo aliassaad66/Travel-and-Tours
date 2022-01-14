7c4ba11a0d035d745371f80a4c3831fd611bed0e
aliassaad66 <alias_saad66@yahoo.com>
2022-01-14 22:48:15 +0800

    book now

--- index.js
+++ index.js
@@ -33,11 +33,13 @@ formClose.addEventListener("click", () =>{
     loginForm.classList.remove("active");
 });
 
-
+// Make Bullet responsive for the video
 videoBtn.forEach(btn =>{
     btn.addEventListener("click", ()=>{
-        $(".controls .active").classList.remove("active");
+        document.querySelector(".controls .active").classList.remove("active");
         btn.classList.add("active");
+        let src = btn.getAttribute("data-src");
+        document.querySelector("#video-slider").src = src;
     });
 });
 
