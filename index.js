bfae8e27e557dda390c66821d6f48dfdb904cb09
aliassaad66 <alias_saad66@yahoo.com>
2022-01-23 19:29:40 +0800

    added review and contact and footer

--- index.js
+++ index.js
@@ -43,9 +43,48 @@ videoBtn.forEach(btn =>{
     });
 });
 
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    },
+});
 
-
-
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        450:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        991:{
+            slidesPerView: 3,
+        },
+        1024:{
+            slidesPerView: 4,
+        },
+    },
+});
 
 
 
